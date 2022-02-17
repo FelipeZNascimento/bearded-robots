39bd29136c6290782cf357dd137d98cc54f616d5
Felipe Nascimento <sharpion.k@gmail.com>
2022-02-17 20:26:35 +0000

    Adding modal and fixing upload button

--- src/components/Tests.tsx
+++ src/components/Tests.tsx
@@ -1,14 +1,35 @@
+import { useState } from "react";
+
+import Backdrop from "@mui/material/Backdrop";
+import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import { DataGrid, GridColDef, GridRowId } from "@mui/x-data-grid";
+import Fade from "@mui/material/Fade";
+import Modal from "@mui/material/Modal";
+import Typography from "@mui/material/Typography";
+import { styled } from "@mui/material/styles";
 import { Upload } from "@mui/icons-material";
+import { DataGrid, GridColDef, GridRowId } from "@mui/x-data-grid";
 
 import classNames from "classnames";
 import styles from "./Tests.module.scss";
 
-function Tests() {
+const Input = styled("input")({
+  display: "none",
+});
+
+const Tests = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalContent, setModalContent] = useState("");
+  const handleModal = (flag: boolean) => setIsModalOpen(flag);
+
   const containerClass = classNames("GFlexCenter", {
     [styles.container]: true,
   });
+
+  const handleModalOpening = (id: GridRowId) => {
+    setModalContent(`Setting content for modal id ${id}`);
+    setIsModalOpen(true);
+  };
 
   const renderButtons = (id: GridRowId) => {
     return (
@@ -17,7 +38,7 @@ function Tests() {
           className={styles.button}
           color="warning"
           variant="outlined"
-          onClick={() => alert(`Viewing id ${id}`)}
+          onClick={() => handleModalOpening(id)}
         >
           View
         </Button>
@@ -94,6 +115,32 @@ function Tests() {
     },
   ];
 
+  const renderModal = () => {
+    return (
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={isModalOpen}
+        onClose={() => handleModal(false)}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={isModalOpen}>
+          <Box className={styles.modal}>
+            <Typography id="transition-modal-title" variant="h6" component="h2">
+              Test Details
+            </Typography>
+            <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+              {modalContent}
+            </Typography>
+          </Box>
+        </Fade>
+      </Modal>
+    );
+  };
   return (
     <section className={containerClass}>
       <div className={styles.table}>
@@ -105,15 +152,20 @@ function Tests() {
           rowsPerPageOptions={[5]}
         />
       </div>
-      <Button
-        variant="contained"
-        startIcon={<Upload />}
-        onClick={() => alert(`Upload`)}
-      >
-        Upload
-      </Button>
+      <label htmlFor="contained-button-file">
+        <Input
+          accept="image/*"
+          id="contained-button-file"
+          multiple
+          type="file"
+        />
+        <Button component="span" variant="contained" startIcon={<Upload />}>
+          Upload
+        </Button>
+      </label>
+      {renderModal()}
     </section>
   );
-}
+};
 
 export default Tests;
