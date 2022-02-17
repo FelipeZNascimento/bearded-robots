0f46436613dc372b19c09e57ed10e5e70f613a60
André Gomes <andre.gomes@sky.uk>
2022-02-17 17:07:17 +0000

    localstorage

---
+++ src/bff/localstorage.tsx
@@ -0,0 +1,30 @@
+const localStorageDB = require('localstoragedb/localstoragedb');
+const md5 = require('md5');
+const TABLE_TESTS = "tests"
+const lib = new localStorageDB("testmanager", localStorage);
+
+export const initDB = () =>  {
+    if(lib.isNew()){
+        lib.createTable(TABLE_TESTS, ["id", "Device", "Steps"]);
+        lib.commit();
+    }
+}
+export const storeTest = (testData:any) => {
+    const newID = md5(JSON.stringify(testData));
+    const check = lib.queryAll(TABLE_TESTS, {
+        query: {id: newID}
+    });
+    if(!check.length){
+        lib.insert(TABLE_TESTS,{id: md5(JSON.stringify(testData)), ...testData})
+        lib.commit();
+    }
+}
+
+export const getTests = () => {
+    return lib.queryAll(TABLE_TESTS);
+}
+
+export const deleteTest = (id: string) => {
+    lib.deleteRows(TABLE_TESTS, {id});
+    lib.commit();
+}
