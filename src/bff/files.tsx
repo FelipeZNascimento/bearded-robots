685b5162a594c70d36aaaf538318c0313e11fbbc
André Gomes <andre.gomes@sky.uk>
2022-02-18 01:49:34 +0000

    manual test validation and some goodies

--- src/bff/files.tsx
+++ src/bff/files.tsx
@@ -1,11 +1,14 @@
 
 export const fileUploadHandler = (e:any, callback:(result: any)=> void) => {
+  for(let i = 0; i < e.target.files.length; i++){
     const reader = new FileReader();
     reader.addEventListener('load', (event) => {
       if(event && event.target && event.target.result){
         callback(JSON.parse(event.target.result as string))
-        e.target.value = null;
       }
     });
-    reader.readAsText(e.target.files[0]);
+    reader.readAsText(e.target.files[i]);
+  }
+  e.target.value = null;
+
 }
