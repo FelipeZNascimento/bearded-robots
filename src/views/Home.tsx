91d12130a36132bd3d7d85165fd69b3c746046ce
Felipe Nascimento <sharpion.k@gmail.com>
2022-02-18 14:40:01 +0000

    Adding tooltips

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,4 +1,5 @@
 import Typography from "@mui/material/Typography";
+import Tooltip from "@mui/material/Tooltip";
 
 import classNames from "classnames";
 import styles from "./Home.module.scss";
@@ -99,9 +100,24 @@ const Home = () => {
         <div className={containerClass}>
             <header className={headerClass}>
                 <div className={styles.bender}>
-                    <img src="/bender.png" alt="Bender robot with a beard" />
-                    <img src="/bender.png" alt="Bender robot with a beard" />
-                    <img src="/bender.png" alt="Bender robot with a beard" />
+                    <Tooltip title="Felipe Nascimento">
+                        <img
+                            src="/bender.png"
+                            alt="Bender robot with a beard"
+                        />
+                    </Tooltip>
+                    <Tooltip title="André Gomes">
+                        <img
+                            src="/bender.png"
+                            alt="Bender robot with a beard"
+                        />
+                    </Tooltip>
+                    <Tooltip title="Hugo Dias">
+                        <img
+                            src="/bender.png"
+                            alt="Bender robot with a beard"
+                        />
+                    </Tooltip>
                 </div>
                 <Typography margin={2} variant="h3">
                     <img src="/sky.png" alt="Sky logo" />
